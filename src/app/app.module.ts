ffa55e572784b703f1423945e031d7ecafb30cf9
Taranjit K <Taranjitk18@gmail.com>
2019-01-24 20:56:56 -0500

    removed hello comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HotkeyModule } from 'angular2-hotkeys';
 
 import { CommandService } from './services/command.service';
-import { HotkeyModule } from 'angular2-hotkeys';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, HttpClientModule, HotkeyModule.forRoot()
   ],
-  declarations: [AppComponent, HelloComponent],
+  declarations: [AppComponent],
   providers: [CommandService],
   bootstrap: [AppComponent]
 })
